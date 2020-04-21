080c3ac1af430f43455727eaafd8cf5c4e5b984c
Babipoki <laughe@laughe-art.com>
2020-04-22 00:04:27 +0300

    More jobs

--- work.py
+++ work.py
@@ -55,7 +55,49 @@ jobs = [
         'minXP': 200,
         'salary': 16,
         'xpPerWork': 8,
-        'customEvents': ["Nothing interesting happned today.", "You hired some great mercenaries today."]
+        'customEvents': ["Nothing interesting happened today.", "You hired some great mercenaries today."]
+    },
+    {
+        'name': 'Bank Owner',
+        'minXP': 500,
+        'salary': 25,
+        'xpPerWork': 10,
+        'customEvents': ["Nothing interesting happened today.", "You had a party after work."]
+    },
+    {
+        'name': "Mayor's Advisor",
+        'minXP': 1000,
+        'salary': 45,
+        'xpPerWork': 15,
+        'customEvents': ["Nothing interesting happened today.", "Mayor threw a great party for you today."]
+    },
+    {
+        'name': "Mayor",
+        'minXP': 5000,
+        'salary': 95,
+        'xpPerWork': 45,
+        'customEvents': ["Nothing interesting happened today.", "You signed way too many bills today."]
+    },
+    {
+        'name': "Baron's Advisor",
+        'minXP': 9500,
+        'salary': 125,
+        'xpPerWork': 95,
+        'customEvents': ["Nothing interesting happened today."]
+    },
+    {
+        'name': "Royal Knight",
+        'minXP': 25000,
+        'salary': 250,
+        'xpPerWork': 145,
+        'customEvents': ["Nothing interesting happened today."]
+    },
+    {
+        'name': "King's Advisor",
+        'minXP': 50000,
+        'salary': 450,
+        'xpPerWork': 250,
+        'customEvents': ["Nothing interesting happened today."]
     }
 ]
 
